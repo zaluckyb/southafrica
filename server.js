25812e84df4f879b1dfaa425ca1b95dc7bc56e05
Gerald Ferreira <zaluckyb@gmail.com>
2025-06-03 11:43:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
-const { createServer } = require('http');
 const next = require('next');
-// const next = require('../node_modules/next');
-
+const http = require('http');
+const { parse } = require('url');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-
-const app = next({ dev, dir: '.' });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-console.log(`📦 Starting Next.js app in ${dev ? 'development' : 'production'} mode...`);
-console.log(`🔌 Using PORT=${port}`);
-
-app.prepare()
-  .then(() => {
-    createServer((req, res) => {
-      handle(req, res);
-    }).listen(port, () => {
-      console.log(`🚀 Server running at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error('❌ Error during app.prepare()', err);
-    process.exit(1); // important: make Passenger fail fast
+app.prepare().then(() => {
+  http.createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
+});
